042b731634300a7517a40247f7723bb6f61a4f0c
joelpdurham <joeldur@gmail.com>
2019-12-19 13:27:50 -0800

    wrote failing test

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -18,4 +18,17 @@ describe('app routes', () => {
   afterAll(() => {
     return mongoose.connection.close();
   });
+
+  it('can signup a user', () => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({ email: 'joel@joel.com', password: 'pass1234' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          email: 'joel@joel.com',
+          __v: 0
+        });
+      });
+  });
 });
